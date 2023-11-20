169366a1595554f097eb989023d285d1cee4824f
Pratikpo <pratik9885@gmail.com>
2023-11-20 19:08:46 +0530

    rest application

---
+++ src/main/java/com/Pratikpo/gettingstartedwithrestapp/restservice/Greeting.java
@@ -0,0 +1,7 @@
+package com.Pratikpo.gettingstartedwithrestapp.restservice;
+
+
+
+public record Greeting(long id, String content) {
+
+}
---
+++ src/test/java/com/Pratikpo/gettingstartedwithrestapp/GettingStartedWithRestAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Pratikpo.gettingstartedwithrestapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GettingStartedWithRestAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
