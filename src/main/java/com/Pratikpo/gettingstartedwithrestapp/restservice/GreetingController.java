a62e46b6bc3413680f2d933d66e921a939d36393
Pratikpo <pratik9885@gmail.com>
2023-11-21 19:21:33 +0530

    rest application

--- src/main/java/com/Pratikpo/gettingstartedwithrestapp/restservice/GreetingController.java
+++ src/main/java/com/Pratikpo/gettingstartedwithrestapp/restservice/GreetingController.java
@@ -4,18 +4,22 @@ package com.Pratikpo.gettingstartedwithrestapp.restservice;
 
 import java.util.concurrent.atomic.AtomicLong;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+
 public class GreetingController {
+    private static final org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(GreetingController.class);
 
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
     @GetMapping("/greeting")
     public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+        log.info("greeting method called....");
         return new Greeting(counter.incrementAndGet(), String.format(template, name));
     }
 }
